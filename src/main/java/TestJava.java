17926a56297a8b353989dc4844b2d3c31fccba19
Jacques <jak.rag@gmail.com>
2018-09-28 14:42:12 +0200

    do partition for all list do partition for list with size negative or null

--- src/main/java/TestJava.java
+++ src/main/java/TestJava.java
@@ -1,18 +1,43 @@
 public class TestJava {
 
     public static int[][] partition(int[] list, int size) {
+        Object[] listObject = new Object[list.length];
+        for (int i = 0; i < list.length; i++) {
+            listObject[i] = list[i];
+        }
+        Object[][] temporaryObjects = partition(listObject, size);
+        int[][] returnList = new int[temporaryObjects.length][];
+        for (int i = 0; i < temporaryObjects.length; i++) {
+            int[] subIntList = new int[temporaryObjects[i].length];
+            for (int i1 = 0; i1 < temporaryObjects[i].length; i1++) {
+                subIntList[i1] = (Integer) temporaryObjects[i][i1];
+            }
+            returnList[i] = subIntList;
+        }
+        return returnList;
+    }
+
+    public static <T> T[][] partition2(T[] list, int size) {
+        Object[][] listObject = partition(list, size);
+        return (T[][]) listObject;
+    }
+
+    public static Object[][] partition(Object[] list, int size) {
+        if (list.length == 0 || size <= 0) {
+            return new Object[1][0];
+        }
         int sizeOfList = getSizeOfList(list.length, size);
-        int[][] returnList = new int[sizeOfList][];
+        Object[][] returnList = new Object[sizeOfList][];
 
         int indexInInputList = 0;
 
         for (int indexInReturnList = 0; indexInReturnList < returnList.length; indexInReturnList++) {
 
             int sizeOfSubList = Math.min(list.length - indexInInputList, size);
-            int[] subList = new int[sizeOfSubList];
+            Object[] subList = new Object[sizeOfSubList];
 
             for (int indexInSubList = 0; indexInSubList < sizeOfSubList; indexInSubList++) {
-                if (indexInInputList == list.length){
+                if (indexInInputList == list.length) {
                     break;
                 }
                 subList[indexInSubList] = list[indexInInputList];
@@ -28,13 +53,11 @@ public class TestJava {
     private static int getSizeOfList(int length, int size) {
         int sizeOfList;
         sizeOfList = length / size;
-        if (length % size > 0){
+        if (length % size > 0) {
             sizeOfList += 1;
-        }
-        if (length == 0){
-            sizeOfList = 1;
         }
         return sizeOfList;
     }
+
 }
 
--- src/test/java/TestJavaTest.java
+++ src/test/java/TestJavaTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TestJavaTest {
+
     @Test
     public void acceptance_test1() {
         int[] list = {1, 2, 3, 4, 5};
@@ -32,40 +33,25 @@ public class TestJavaTest {
     }
 
     @Test
-    public void partition_of_a_list_with_one_element_and_one_for_the_size_should_return_the_same_list() {
-        int[] list = {1};
-        int size = 1;
+    public void partition_should_return_a_list_of_the_input_list_when_size_equal_length() {
+        int[] list = {1, 2, 3, 4, 5};
+        int size = list.length;
 
-        int[][] listExpected = {{1}};
+        int[][] listExpected = {{1, 2, 3, 4, 5}};
         assertThat(TestJava.partition(list, size), is(listExpected));
     }
-    @Test
-    public void partition_of_a_list_with_elements_and_the_list_size_for_the_size_should_return_the_same_list() {
-        int[] list = {1,2,3,4,5};
-        int size = 5;
 
-        int[][] listExpected = {{1,2,3,4,5}};
-        assertThat(TestJava.partition(list, size), is(listExpected));
-    }
     @Test
-    public void partition_of_a_list_with_n_elements_and_n_divide_by_2_for_the_size() {
-        int[] list = {1,2,3,4};
+    public void partition_should_return_a_list_of_two_list_when_size_is_half_of_length() {
+        int[] list = {1, 2, 3, 4};
         int size = 2;
 
-        int[][] listExpected = {{1,2},{3,4}};
+        int[][] listExpected = {{1, 2}, {3, 4}};
         assertThat(TestJava.partition(list, size), is(listExpected));
     }
 
     @Test
-    public void partition_of_a_list_with_3_elements_and_2_for_the_size() {
-        int[] list = {1, 2, 3};
-        int size = 2;
-
-        int[][] listExpected = {{1, 2}, {3}};
-        assertThat(TestJava.partition(list, size), is(listExpected));
-    }
-    @Test
-    public void partition_of_a_list_with_3_elements_and_1_for_the_size() {
+    public void partition_should_return_a_list_of_list_of_single_element_when_size_is_1() {
         int[] list = {1, 2, 3};
         int size = 1;
 
@@ -74,15 +60,25 @@ public class TestJavaTest {
     }
 
     @Test
-    public void partition_of_a_list_with_6_elements_and_2_for_the_size() {
+    public void partition_should_return_a_list_of_list_with_length_equal_size_when_list_length_is_divisible_by_size() {
         int[] list = {1, 2, 3, 4, 5, 6};
         int size = 2;
 
-        int[][] listExpected = {{1,2}, {3,4}, {5,6}};
+        int[][] listExpected = {{1, 2}, {3, 4}, {5, 6}};
         assertThat(TestJava.partition(list, size), is(listExpected));
     }
+
     @Test
-    public void partition_of_an_empty_list_and_1_for_the_size() {
+    public void partition_should_return_a_list_of_lists_whose_length_is_equal_or_less_than_size() {
+        int[] list = {1, 2, 3};
+        int size = 2;
+
+        int[][] listExpected = {{1, 2}, {3}};
+        assertThat(TestJava.partition(list, size), is(listExpected));
+    }
+
+    @Test
+    public void partition_should_return_a_list_of_an_empty_list_when_the_input_list_is_empty() {
         int[] list = {};
         int size = 1;
 
@@ -91,6 +87,43 @@ public class TestJavaTest {
     }
 
 
+    @Test
+    public void partition_should_return_a_list_of_the_input_list_when_size_is_bigger_than_length() {
+        int[] list = {1, 2, 3, 4, 5, 6, 7};
+        int size = 8;
+
+        int[][] listExpected = {{1, 2, 3, 4, 5, 6, 7}};
+        assertThat(TestJava.partition(list, size), is(listExpected));
+    }
+
+    @Test
+    public void partition_should_return_a_list_of_an_empty_list_when_size_is_negative_or_equal_zero() {
+        int[] list = {1, 2, 3, 4, 5};
+        int sizeZero = 0;
+        int sizeNegative = -5;
+
+        int[][] listExpected = {{}};
+        assertThat(TestJava.partition(list, sizeZero), is(listExpected));
+        assertThat(TestJava.partition(list, sizeNegative), is(listExpected));
+    }
+
+
+    @Test
+    public void partition_of_a_list_with_5_object_and_2_for_the_size() {
+        Object[] list = {"1", "2", "3", "4", "5"};
+        int size = 2;
+
+        Object[][] listExpected = {{"1","2"}, {"3","4"}, {"5"}};
+        assertThat(TestJava.partition(list, size), is(listExpected));
+    }
+    @Test
+    public void partition_of_a_list_with_5_generics_object_and_2_for_the_size() {
+        String[] list = {"1", "2", "3", "4", "5"};
+        int size = 2;
+
+        String[][] listExpected = {{"1","2"}, {"3","4"}, {"5"}};
+        assertThat(TestJava.partition2(list, size), is(listExpected));
+    }
 
 
 }
